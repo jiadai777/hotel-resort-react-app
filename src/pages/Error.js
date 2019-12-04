0133a8946b7a790fd42b9dafb99bc5e594a13ef3
Jason Liu <jliu@distroscale.com>
2019-12-03 16:19:27 -0800

    created featured rooms component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            hello from Error
-        </div>
+        <Hero>
+            <Banner title="404" subtitle="page not found">
+                <Link to="/" className="btn-primary">return home</Link>
+            </Banner>
+        </Hero>
     )
 }
 
