587b0282c10dbd7a43cbcf57b5f001bcc11e32b2
Jason Liu <jliu@distroscale.com>
2019-12-06 16:48:51 -0800

    added single room info

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route exact path="/single-room" component={SingleRoom} />
         <Route component={Error} />
       </Switch>
